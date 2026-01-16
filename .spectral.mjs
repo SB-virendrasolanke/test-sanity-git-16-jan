052b016974dbad1b37c52c40ffe15169bbcb8040
virendra.solanke@smartbear.com <virendra.solanke@smartbear.com>
2026-01-16 10:24:48 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwMzM5NQ/spectral.js?branch_slug=main&token=1bece5cb-742e-486d-854b-5fc07d55fbfe";
+export default { extends: ruleset };
